3f7df1ea477b1d50f61c944d722993cea68550ef
Coder-Maxxx <148607705+Coder-Maxxx@users.noreply.github.com>
2023-11-14 13:58:26 +0800

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+
+int c = 0;
+
+int func1(int x)
+{
+    static int c = 0;
+    x = ++c;
+    return x;
+}
+
+int func2(int *x)
+{
+    static int c = 0;
+    *x = ++c;
+    return *x;
+}
+
 int main()
 {
-    printf("hello the world");
+    int a = 0;
+
+    a = func1(c);
+    printf("%d%d\n", a, c);
+
+    a = func2(&c);
+    printf("%d%d\n", a, c);
+
+    a = func1(c);
+    printf("%d%d\n", a, c);
+
+    a = func2(&c);
+    printf("%d%d\n", a, c);
+
     return 0;
 }
