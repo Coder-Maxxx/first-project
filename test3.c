e71a871673bf4351e9424d2a26e66250db041916
Coder-Maxxx <148607705+Coder-Maxxx@users.noreply.github.com>
2023-11-14 09:18:42 +0800

    test github

---
+++ test3.c
@@ -0,0 +1,13 @@
+int main()
+{
+    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // printf ("%d\n", arr[8]);
+    int i = 0;
+    while (i < 10)
+    {
+        printf("%d\n", arr[i]);
+        i = i + 1;
+    }
+
+    return 0;
+}
